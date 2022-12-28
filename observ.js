4769c5c0e7d76eff8715b13deb19fb1211a32bbd
markoleg <94861029+markoleg@users.noreply.github.com>
2022-12-28 22:13:53 +0200

    www

--- observ.js
+++ observ.js
@@ -1,9 +1,11 @@
 
 const header = document.querySelector("header");
+const social = document.querySelector(".social");
+const navigation = document.querySelector(".navigation");
 const sectionOne = document.querySelector(".home-intro");
 
 const sectionOneOptions = {
-  rootMargin: "-100px 0px 0px 0px"
+  rootMargin: "-500px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(
@@ -13,8 +15,12 @@ const sectionOneObserver = new IntersectionObserver(function(
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       header.classList.add("nav-white");
+      social.classList.add("social-white")
+      navigation.classList.add("white")
     } else {
       header.classList.remove("nav-white");
+      social.classList.remove("social-white")
+      navigation.classList.remove("white")
     }
   });
 },
